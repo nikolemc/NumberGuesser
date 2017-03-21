21dc7cea0ced42b3dfba5b6bf816b3566f59a05a
nikolemc <nikolemcstanley@gmail.com>
2017-03-21 13:05:38 -0400

    Generate a random number between 1 and 100.  Prompt the user for a guess.

--- NumberGuesser/NumberGuesser/Program.cs
+++ NumberGuesser/NumberGuesser/Program.cs
@@ -10,6 +10,25 @@ namespace NumberGuesser
     {
         static void Main(string[] args)
         {
+            //Level 1
+            //Generate a random number between 1 and 100
+            //Prompt the user for a guess
+            //If the guess is less than the program's number, it should tell you that you were low and let you guess again.
+            //If the guess is greater than the program's number, it should tell you that you were high and let you guess again.
+            //If the guess is correct, the program should tell you that you win and then quit.
+            //After 5 incorrect guesses, the program should tell you that you lose.
+            //You should display all the guesses to the screen
+
+            Random random = new Random();
+            int randomNumber = random.Next(0, 100);
+
+            Console.WriteLine("Guess a number between 1 and 100?:");
+            string line = Console.ReadLine();
+         
+
+          
+
+
         }
     }
 }
